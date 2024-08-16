5fe8b099f37cbd609975e86beccd8e51de881535
Farouk Mestiri <117433957+Mestiri77@users.noreply.github.com>
2024-08-16 16:26:43 +0100

    Create warmup01.js

---
+++ warmup01.js
@@ -0,0 +1,46 @@
+
+/**
+ * 1-Given an array of integers and an integer n.
+ * find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
+ * If n is not in the given array, return an empty array [].
+ * Assume that n and all values in the given array will always be integers.
+ * Example:
+ * findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
+ *
+ * @param {Array} array
+ * @param {number} n
+ * @return {Array}
+ */
+let findAll = function (array, n) {
+  
+};
+
+
+// 2. write a function that takes a string as an argument
+// and returns the string reversed word by word
+// Example : reverseWord("hi there how is the weather today") ==>> "today weather the is how there hi"
+//
+//
+// Note : you can`t use any built-in functions .
+
+let reverseWord=(string) => {
+
+}
+
+
+/**
+ * 3 - write a function called (sameLength) that takes array of strings and returns an array of the strings that have the same length
+ * example :  ['hi','hello','welcome','hy'] ==> ["hi","hy"]
+ *
+ * @param {Array<string>} array
+ * @return {Array<string>}
+ */
+
+let sameLength=(array) => {
+
+}
+
+  
+function sameLength(array) {
+  
+}
